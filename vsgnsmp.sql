2b72ac668ac2b3f1271670f01174de2925306610
S23000584 <s23000584@ousl.lk>
2024-12-29 23:52:35 +0530

    Add files via upload

---
+++ vsgnsmp.sql
@@ -0,0 +1,220 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 29, 2024 at 02:26 PM
+-- Server version: 8.0.39
+-- PHP Version: 8.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `vsgnsmp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `announcements`
+--
+
+DROP TABLE IF EXISTS `announcements`;
+CREATE TABLE IF NOT EXISTS `announcements` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `summary` text,
+  `date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `announcements`
+--
+
+INSERT INTO `announcements` (`id`, `title`, `summary`, `date`) VALUES
+(1, 'Assignment Reminder', 'Assignment 2 is due on Jan 5, 2025. Submit via the LMS portal.', '2024-12-29 07:18:02'),
+(2, 'Class Cancelled', 'Tomorrow\'s lecture on \"Data Structures\" has been cancelled. Updates will follow.', '2024-12-29 07:31:19'),
+(3, 'System Update', 'LMS will be offline for maintenance on Jan 7, 2025, from 1:00 AM to 5:00 AM.', '2024-12-29 07:24:33');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+DROP TABLE IF EXISTS `messages`;
+CREATE TABLE IF NOT EXISTS `messages` (
+  `message_id` int NOT NULL AUTO_INCREMENT,
+  `sender_id` int NOT NULL,
+  `recipient_id` int NOT NULL,
+  `content` text NOT NULL,
+  `sent_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `read_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`message_id`),
+  KEY `sender_id` (`sender_id`),
+  KEY `recipient_id` (`recipient_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `social_media_groups`
+--
+
+DROP TABLE IF EXISTS `social_media_groups`;
+CREATE TABLE IF NOT EXISTS `social_media_groups` (
+  `group_id` int NOT NULL AUTO_INCREMENT,
+  `course_name` varchar(100) DEFAULT NULL,
+  `platform` enum('facebook','linkedin') NOT NULL,
+  `group_link` varchar(255) DEFAULT NULL,
+  `created_by` int DEFAULT NULL,
+  PRIMARY KEY (`group_id`),
+  KEY `created_by` (`created_by`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `study_groups`
+--
+
+DROP TABLE IF EXISTS `study_groups`;
+CREATE TABLE IF NOT EXISTS `study_groups` (
+  `group_id` int NOT NULL AUTO_INCREMENT,
+  `group_name` varchar(100) NOT NULL,
+  `description` text,
+  `created_by` int DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`group_id`),
+  KEY `created_by` (`created_by`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `study_group_members`
+--
+
+DROP TABLE IF EXISTS `study_group_members`;
+CREATE TABLE IF NOT EXISTS `study_group_members` (
+  `group_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `joined_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`group_id`,`user_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `role` enum('admin','instructor','student') NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `date_joined` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `password`, `email`, `role`, `first_name`, `last_name`, `date_joined`) VALUES
+(1, 'admin', 'admin', 'admin@ou.ac.lk', 'admin', 'System', 'Admin', '2024-12-29 02:03:55'),
+(7, 'S001', 'S001', 's001@ousl.lk', 'student', 'D.S.R', 'Perera', '2024-12-29 02:06:16'),
+(8, 'S002', 'S002', 's002@ousl.lk', 'student', 'K.D.B.C', 'Fernando', '2024-12-29 02:07:40');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `workshops`
+--
+
+DROP TABLE IF EXISTS `workshops`;
+CREATE TABLE IF NOT EXISTS `workshops` (
+  `workshop_id` int NOT NULL AUTO_INCREMENT,
+  `workshop_name` varchar(100) DEFAULT NULL,
+  `workshop_description` text,
+  `start_time` timestamp NULL DEFAULT NULL,
+  `created_by` int DEFAULT NULL,
+  PRIMARY KEY (`workshop_id`),
+  KEY `created_by` (`created_by`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `workshop_zoom_links`
+--
+
+DROP TABLE IF EXISTS `workshop_zoom_links`;
+CREATE TABLE IF NOT EXISTS `workshop_zoom_links` (
+  `workshop_id` int NOT NULL,
+  `zoom_link` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`workshop_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`sender_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`recipient_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `social_media_groups`
+--
+ALTER TABLE `social_media_groups`
+  ADD CONSTRAINT `social_media_groups_ibfk_1` FOREIGN KEY (`created_by`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `study_groups`
+--
+ALTER TABLE `study_groups`
+  ADD CONSTRAINT `study_groups_ibfk_1` FOREIGN KEY (`created_by`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `study_group_members`
+--
+ALTER TABLE `study_group_members`
+  ADD CONSTRAINT `study_group_members_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `study_groups` (`group_id`),
+  ADD CONSTRAINT `study_group_members_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `workshops`
+--
+ALTER TABLE `workshops`
+  ADD CONSTRAINT `workshops_ibfk_1` FOREIGN KEY (`created_by`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `workshop_zoom_links`
+--
+ALTER TABLE `workshop_zoom_links`
+  ADD CONSTRAINT `workshop_zoom_links_ibfk_1` FOREIGN KEY (`workshop_id`) REFERENCES `workshops` (`workshop_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
